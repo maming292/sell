16ba0ca24f4f7d8fdad452efd749f5cb4c165d5e
Mario <maming292@163.com>
2018-04-11 17:29:56 +0800

    2018-4-11 seller

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -52,21 +52,30 @@
   @import "../../common/stylus/mixin.styl"
   .seller
     position absolute
-      top: 174px
-      bottom 0
-      left 0
-      width 100%
-      overflow hidden
-      .overview
-        padding 18px
-        .title
-          margin-bottom 8px
-          line-height 14px
-          color rgb(7, 17, 27)
-          font-size 14px
-        .desc
-          padding-bottom 18px
-          line-height 18px
-          border-1px(rgba(7, 17, 27, 0.1))
-          font-size 14px
+    top: 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
+    .overview
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        color rgb(7, 17, 27)
+        font-size 14px
+      .desc
+        padding-bottom 18px
+        line-height 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 14px
+        .star
+          display inline-block
+          margin-right 8px
+          vertical-align top
+        .text
+          margin-right 12px
+          display inline-block
+          vertical-align top
+          color rgb(77, 85, 93)
 </style>
